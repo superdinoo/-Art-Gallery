032217e081dd922eb53d25af269a64a16707a40a
Your Name <serzh.bakalov.81@mail.ru>
2025-01-29 01:36:16 +0400

    fix deploy 2

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-import baseUrl from "./src/baseUrl";
 
 export default defineConfig({
-  base: baseUrl,
+  base: "https://superdinoo.github.io/-Art-Gallery/",
   plugins: [react(), svgr()],
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx"],
