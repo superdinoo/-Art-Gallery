d441be9c6896b3bdc3cad5505c09bd241518ee36
Your Name <serzh.bakalov.81@mail.ru>
2025-01-29 01:28:44 +0400

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import baseUrl from "./src/baseUrl";
 
 export default defineConfig({
+  base: baseUrl,
   plugins: [react(), svgr()],
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx"],
